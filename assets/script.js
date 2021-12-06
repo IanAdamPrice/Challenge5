855333dc4e7f0b5ebb2e7356ce0a5f2dba674110
IanPrice <ianadamprice@gmail.com>
2021-12-05 23:50:38 -0500

    included all functions to create each hour div

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,88 @@
-var currentDateEl = document.querySelector('#currentDay');
+var liveDateEl = $("#currentDay");
 
-var liveDate = function() {
-    const today = moment();
-    currentDateEl.innerHTML = today.format('dddd, MMMM do YYYY');
+hour = moment();
+
+function Calendar(today, calEvents) {
+    var rowHour = moment(today).hour(9);
+    var calendar = $("div.container"); 
+        
+    calendar.empty(); 
+
+    // loop 9 times to create each hour    
+    for (let i = 0; i < 9; i++) {
+        var row = $("<div>")
+          .addClass("row"); 
+
+        var classColor = "";
+        if (today.isBefore(rowHour, "hour")) {
+            classColor = "future"
+        } else if (today.isAfter(rowHour, "hour")) {
+            classColor = "past"
+        } else {
+            classColor = "present"
+        };
+
+        calendar.append(row);
+           
+        row.append($("<div>")
+          .addClass("col-1 hour")
+          .text(rowHour.format("h A")));
+            
+        var timeBlock = rowHour.format("hA");
+
+        row.append($("<textarea>")
+          .addClass(`col-10 ${classColor}`)
+          .text(calEvents[timeBlock]));
+
+        row.append($("<button>")
+          .addClass("col-1 saveBtn")
+          .html("<i class='fas fa-save'></i>")
+          .attr("aria-label", "Save")
+          .attr("id", rowHour.format("hA")));
+
+        
+        rowHour.add(1, "hour");
+
+        hour = moment();
+    };
 };
 
-liveDate();
+function createCalendar() {
+    var today = moment(); 
+    liveDateEl.text(today.format('LLLL')); //set current day
+    Calendar(today, calEvents);
+};
+
+function loadCal() {
+    storedCal = JSON.parse(localStorage.getItem("calEvents"));
+    if (storedCal) {
+        calEvents = storedCal;
+    };
+};
+ 
+loadCal(); 
+createCalendar(); 
+hourTracker();
+
+
+function hourTracker() {
+    var checkHourInterval = setInterval(function () {
+        if (moment().isAfter(hour, "minute")) {
+            createCalendar(); 
+        }
+    }, 60000);
+};
+
+
+// store calendar events in local storage
+function storeCal() {
+    localStorage.setItem("calEvents", JSON.stringify(calEvents));
+}
+
+    
+$(document).on("click", "button.saveBtn", function (event) {
+    calDesc = event.currentTarget.parentElement.children[1].value; 
+    calEvents[event.currentTarget.id] = calDesc; 
+    storeCal(); 
+});
+
